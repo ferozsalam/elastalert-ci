1c2644526edd940c0c3b1c32df1f4df35f0a6be6
Feroz Salam <feroz@argh.in>
2020-10-13 21:45:40 +0100

    Refactor to remove CI logic from detection rules
    This allows all the CI logic to be put in a single file, which
    is tidier, and also allows for an n-to-n relationship between data
    files and the rules that are tested against them.

--- elastalert-ci.py
+++ elastalert-ci.py
@@ -52,45 +52,47 @@ def rule_matched(raw_elastalert_output):
 
     return output["writeback"]["elastalert_status"]["matches"] > 0
 
-def check_rule(rule, data_config):
-    print(f'Testing {rule.get("name", "[rule name not found]")}')
-    try:
-        data_source = rule["ci_data_source"]
-    except:
-        print("No CI definition for rule")
-        raise
-
-    rewrite_rule(rule, data_config[data_source])
-
-    filename = data_config['directory'] + '/' + data_config[data_source]["filename"]
-    data = open(filename, 'rb').read()
-    try:
-        load_es_data(data)
-    except requests.exceptions.HTTPError as e:
-        print(f"Failed to load data from {filename} into Elasticsearch, exiting")
-        exit(1)
-
-    elastalert_run = subprocess.run(["elastalert-test-rule",
-                                      "--formatted-output",
-                                      "--config", "/data/config.yaml",
-                                      "--start", data_config[data_source]["start_time"],
-                                      "--end", data_config[data_source]["end_time"],
-                                      "rule_rewritten.yaml"], 
-                                      capture_output=True,
-                                      text=True,
-                                      check=True)
-
-    try:
-        clear_test_index()
-    except requests.exceptions.HTTPError:
-        print("Failed to remove data from Elasticsearch, exiting")
-        exit(1)
-
-    alert_fired = re.search(":Alert for", elastalert_run.stderr)
-    if (alert_fired and rule_matched(elastalert_run.stdout)):
-        return True
+def check_rule(rule, data_config, data_directory):
+    if "name" in rule:
+        print(f'Testing {rule.get("name")}')
     else:
-        return False
+        print('No rule name found, skipping YAML file')
+        return True
+
+    data_sources = filter(lambda x : rule["name"] in data_config[x]["rules"], data_config)
+
+    for data_source in data_sources:
+        rewrite_rule(rule, data_config[data_source])
+
+        filename = data_directory + '/' + data_config[data_source]["filename"]
+        data = open(filename, 'rb').read()
+        try:
+            load_es_data(data)
+        except requests.exceptions.HTTPError as e:
+            print(f"Failed to load data from {filename} into Elasticsearch, exiting")
+            exit(1)
+
+        elastalert_run = subprocess.run(["elastalert-test-rule",
+                                          "--formatted-output",
+                                          "--config", "/data/config.yaml",
+                                          "--start", data_config[data_source]["start_time"],
+                                          "--end", data_config[data_source]["end_time"],
+                                          "rule_rewritten.yaml"],
+                                          capture_output=True,
+                                          text=True,
+                                          check=True)
+
+        try:
+            clear_test_index()
+        except requests.exceptions.HTTPError:
+            print("Failed to remove data from Elasticsearch, exiting")
+            exit(1)
+
+        alert_fired = re.search(":Alert for", elastalert_run.stderr)
+        if (alert_fired and rule_matched(elastalert_run.stdout)):
+            return True
+        else:
+            return False
 
 def main():
     parser = argparse.ArgumentParser()
@@ -101,22 +103,20 @@ def main():
 
     with open(args.data) as data_config_file:
         data_config = yaml.safe_load(data_config_file)
-    data_config['directory'] =  os.path.dirname(args.data)
 
     passed_rules = []
     failed_rules = []
-
     for rule_filename in args.rules:
         # Load rule to test against
         with open(rule_filename) as rule_file:
             rule = yaml.safe_load(rule_file)
-        
+
         try:
-            if (check_rule(rule, data_config)):
+            if (check_rule(rule, data_config, os.path.dirname(args.data))):
                 passed_rules.append(rule["name"])
             else:
                 failed_rules.append(rule["name"])
-        except Exception:
+        except:
             print(f"YAML file failed rule checks, skipping")
             continue
 
